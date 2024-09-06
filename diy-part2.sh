be69a29deab55de08a7ea845bf021190d04014cc
645916991 <77471759+645916991@users.noreply.github.com>
2024-09-06 14:51:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,16 +9,16 @@ sed -i 's/OpenWrt/OWrt/g' package/base-files/files/bin/config_generate
 
 # 设置wifi加密方式为psk2+ccmp,wifi密码为88888889
 sed -i 's/encryption=none/encryption=psk2+ccmp/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i '/set wireless.default_radio${devidx}.encryption=psk2+ccmp/a\\t\t\tset wireless.default_radio${devidx}.key=88888889' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/set wireless.default_radio${devidx}.encryption=psk2+ccmp/a\\t\t\tset wireless.default_radio${devidx}.key=QQ858952311' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 设置无线的国家代码为CN,wifi的默认功率为20
 sed -i 's/country=US/country=CN/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i '/set wireless.radio${devidx}.disabled=0/a\\t\t\tset wireless.radio${devidx}.txpower=20' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 设置wan口上网方式为PPPOE，本地编译时在文件的第86行左右
-# sed -i 's/2:-dhcp/2:-pppoe/g' package/base-files/files/lib/functions/uci-defaults.sh
+ sed -i 's/2:-dhcp/2:-pppoe/g' package/base-files/files/lib/functions/uci-defaults.sh
 # 设置PPPOE上网的账号和密码,本地编译时在文件的第182和183行左右
-# sed -i 's/username='"'"'username'"'"'/username='"'"'403'"'"'/g; s/password='"'"'password'"'"'/password='"'"'8888'"'"'/g' package/base-files/files/bin/config_generate
+ sed -i 's/username='"'"'username'"'"'/username='"'"'601'"'"'/g; s/password='"'"'password'"'"'/password='"'"'18639812928'"'"'/g' package/base-files/files/bin/config_generate
 
 # 设置默认开启MU-MIMO
 # sed -i '/set wireless.radio${devidx}.disabled=0/a\\t\t\tset wireless.radio${devidx}.mu_beamformer=1' package/kernel/mac80211/files/lib/wifi/mac80211.sh
